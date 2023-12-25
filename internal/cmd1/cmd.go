2864293f47f8b78f558f52a8458708acbac66752
trrrrrys <tsukahara-ryo@dmm.com>
2023-12-25 16:47:46 +0900

    add

--- internal/cmd1/cmd.go
+++ internal/cmd1/cmd.go
@@ -1,8 +1,14 @@
 package cmd1
 
-import "log"
+import (
+	"errors"
+	"log"
+)
 
 func Command1(n int) error {
+	if n == 0 {
+		return errors.New("cmd1.Func1: n is zero")
+	}
 	if n%2 == 0 {
 		log.Println("cmd1.Func1: n is even")
 	} else if n%3 == 0 {
